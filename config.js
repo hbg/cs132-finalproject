2a60aba56a2db1e77c503380295691af3c2ebb1a
Harris Beg <harris@harrisbeg.com>
2024-06-06 13:17:19 -0700

    Heroku config params

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    PORT: 8889,
-    USER: "root",
-    PASSWORD: "root",
-    DATABASE: "storedb",
+	HOST: "us-cluster-east-01.k8s.cleardb.net",
+    PORT: 3306,
+    USER: "b21d50387c9fb1",
+    PASSWORD: "f66239ef",
+    DATABASE: "heroku_8ad1b008d1bd7df",
     SESSION_KEY: '6c352fed-a86a-4cf8-bdd7-1b442a3d290b'
 };
